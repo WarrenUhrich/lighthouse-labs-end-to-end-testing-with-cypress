7eb42690047957a12891c21b35cf675c4f30a702
Warren <hello@warren.codes>
2022-03-17 12:47:30 -0600

    Add keypress test for search field!

--- cypress/integration/03_input.spec.js
+++ cypress/integration/03_input.spec.js
@@ -3,17 +3,20 @@ describe('tests for the "search" input field', () => {
     // Use this to be DRY! Don't repeat yourself!
     beforeEach(() => {
         cy.visit('http://localhost:3000');
+        cy.get('[type="text"]')
+            .as('searchInput'); // Alias! Do this to reference this element later.
+            // https://docs.cypress.io/guides/core-concepts/variables-and-aliases
     });
 
     it('can type into the "search" input field', () => {
-        cy.get('[type="text"]')
+        cy.get('@searchInput') // Retrieving from ALIAS @searchInput
             .type('Daft Punk')
             .should('have.value', 'Daft Punk');
     });
    
     it('can use backspaces in "search" input field', () => {
-        cy.get('[type="text"]')
-            .type('Daft Punk')
-            .should('have.value', 'Daft Punk');
+        cy.get('@searchInput') // Keypresses are in curly braces.
+            .type('Dart{backspace}{backspace}ft Punk', {delay: 200})
+            .should('have.value', 'Daft Punk'); // The backspaces worked!
     });
 });
