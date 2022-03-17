5962d05e7243bc34f80ba613744a94b9ab795976
Warren <hello@warren.codes>
2022-03-17 13:08:16 -0600

    Attempt display results test - data wasn't grabbed fast enough?

--- cypress/integration/04_display-results.spec.js
+++ cypress/integration/04_display-results.spec.js
@@ -4,11 +4,19 @@ describe('test result display for album searches', () => {
         cy.visit('http://localhost:3000');
 
         // "Intercept" the Axios/AJAX request, and replace with test/fake data.
+        cy.intercept(
+            'GET', // METHOD
+            '**/search*', // WEB ADDRESS MATCHING (* = wildcard)
+            {fixture: 'itunes'} // Replace response with OUR itunes data.
+        );
 
         // Find the input field, and type in a search.
         cy.get('[type="text"]')
             .type('Daft Punk');
 
         // Check if a particular album is present.        
+        cy.get('.album') // FAILED! This happens before the data is loaded...
+            // .contains('TRON: Legacy Reconfigured')
+            .should('contain', 'TRON: Legacy Reconfigured');
     });
 });
