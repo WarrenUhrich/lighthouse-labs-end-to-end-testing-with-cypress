8894ce0d4dcbf282252f68579d5ae9e46a280681
Warren <hello@warren.codes>
2022-08-04 12:33:16 -0600

    Add alias for search input example.

--- cypress/integration/03-input.spec.js
+++ cypress/integration/03-input.spec.js
@@ -4,16 +4,19 @@ describe('tests for the search input field', () => {
 
     beforeEach(() => {
         cy.visit('/');
+
+        cy.get('[name="search"]')
+          .as('searchInput'); // Set an alias to represent this selection.
     });
 
     it('can type into the search field', () => {
-        cy.get('[name="search"]')
+        cy.get('@searchInput') // Alias' are called with an @ symbol.
           .type('They Might be Giants', {delay: 150})
           .should('have.value', 'They Might be Giants');
     });
 
     it('can backspace in the search field', () => {
-        cy.get('[name="search"]')
+        cy.get('@searchInput')
           .type('Human Leagjksfdf{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}ue', {delay: 150})
           .should('have.value', 'Human League');
     });
